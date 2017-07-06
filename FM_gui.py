07a8ca2a175a9ee77063f7f1ab3023c8d97635b4
popreiz <Chaiyahcat666@gmail.com>
2017-07-06 18:24:12 +0700

    update function & GUI

--- FM_gui.py
+++ FM_gui.py
@@ -1,4 +1,18 @@
-import maya.cmds as mc
+### test sys ###
+import getpass
+import sys
+user = getpass.getuser()
+path = "C:/Users/"+user+"/Documents/yggSaveGUI"
+if not path in sys.path:
+    sys.path.append(path)
+    
+import package.FM_function as func
+reload(function)
+
+f = func.fileManager()
+
+f.display()
+
 
 if cmds.window('saveWindow' , q=True, ex=True):
     cmds.deleteUI('saveWindow' , window=True)
@@ -6,46 +20,60 @@ cmds.window('saveWindow' , t='Save / Open File')
 cmds.columnLayout('projPath' ,adjustableColumn = True,cal='left')
 cmds.rowLayout('pathLayout', numberOfColumns=3, columnWidth3=(80, 75, 150), adjustableColumn=2, columnAlign=(1, 'left'), columnAttach=[(1, 'left', 0), (2, 'left', 0), (3, 'left', 0)] )
 cmds.text('path : ')
-cmds.textField('path' , text = 'C/:...')
-cmds.button(l = 'set as default path' , c = 'default')
+cmds.text('path' , l = f.realTimePath ,backgroundColor = [0.18,0.18,0.18])
+#cmds.button(l = 'set as default path' , c = f.set_default)
 cmds.setParent('..')
 cmds.separator()
-cmds.rowLayout( 'taskLayout',numberOfColumns=3, columnWidth3=(80, 75, 150), adjustableColumn=2, columnAlign=(1, 'left'), columnAttach=[(1, 'left', 0), (2, 'left', 0), (3, 'left', 0)] )
+cmds.rowLayout( 'taskLayout',numberOfColumns=2, columnWidth3=(80, 75, 150), adjustableColumn=2, columnAlign=(1, 'left'), columnAttach=[(1, 'left', 0), (2, 'left', 0), (3, 'left', 0)] )
 cmds.text('taskName : ')
 cmds.textField('taskName' , text = 'master')
-cmds.checkBox( label = "auto update version", onCommand = "on_func", offCommand = "off_func",value =1)
+#cmds.checkBox( label = "auto update version", onCommand = f.updateOn, offCommand = f.updateOff,value =1)
 cmds.setParent('..')
 
 
 
-cmds.columnLayout('menu2' ,adjustableColumn = True)
 cmds.rowLayout('sec',adjustableColumn=2 , numberOfColumns=2)
 
 cmds.columnLayout('projPath' ,adjustableColumn = True,cal='left')
-cmds.optionMenuGrp( label='project : ' )
-cmds.menuItem( label='Red' )
-cmds.menuItem( label='Green' )
-cmds.separator()
-cmds.optionMenuGrp( label='S/A : ' )
-cmds.menuItem( label='Red' )
-cmds.menuItem( label='Green' )
-cmds.separator()
-cmds.optionMenuGrp( label='sequences/assets : ' )
-cmds.menuItem( label='Red' )
-cmds.menuItem( label='Green' )
-cmds.separator()
-cmds.optionMenuGrp( label='shot/name : ' )
-cmds.menuItem( label='Red' )
-cmds.menuItem( label='Green' )
-cmds.separator()
-cmds.optionMenuGrp( label='dept : ' )
-cmds.menuItem( label='Red' )
-cmds.menuItem( label='Green' )
+
+cmds.optionMenu(['projM'], label='project : ',changeCommand=f.set_proj )
+cmds.menuItem( 'noneproj',label='none', p = 'projM' , en=False )
+for x in f.listRealTimePath:
+    cmds.menuItem(x , l = x , p = 'projM' )
+cmds.separator(h=30)
+
+cmds.optionMenu(['sa'], label='S/A : ' ,en=0 ,changeCommand=f.set_sa)
+cmds.menuItem( 'nonesa',label='none', p = 'sa', en=False )
+cmds.separator(h=30)
+
+cmds.optionMenu(['seqAss'], label='sequences/assets : ' , en=0 , changeCommand=f.set_seqAss)
+cmds.menuItem( 'noneseq',label='none', p = 'seqAss', en=False )
+cmds.separator(h=30)
+
+cmds.optionMenu(['shotName'], label='shot/name : ', en=0 , changeCommand=f.set_shtName )
+cmds.menuItem( 'noneshot',label='none', p = 'shotName' , en=False)
+cmds.separator(h=30)
+
+cmds.optionMenu( ['dept'],label='dept : ', en=0 , changeCommand=f.set_dept )
+cmds.menuItem( 'nonedept',label='none', p = 'dept', en=False )
 cmds.setParent('..')
 
-cmds.frameLayout ("Name", label = "Name" )
-cmds.iconTextScrollList(allowMultiSelection=True, append=('sphere.png','two','three'))
+cmds.columnLayout('menu2' ,adjustableColumn = True,cal='right')
+cmds.paneLayout(configuration = 'vertical3')
+cmds.frameLayout ("Name", label ="Name" )
+cmds.iconTextScrollList('displayN', append=(f.listRealTimePath),h=200 ,doubleClickCommand=f.enterPath , selectCommand=f.sel )
 cmds.setParent('..')
+cmds.frameLayout ("Date", label ="Date Modiefied" )
+cmds.iconTextScrollList('displayD',allowMultiSelection=True,en=0, append=(f.fileDate),h=200 )
+cmds.setParent('..')
+cmds.frameLayout ("Size", label ="Size" )
+cmds.iconTextScrollList('displayS',allowMultiSelection=True,en=0, append=(f.fileSize),h=200 )
+cmds.setParent('..')
+cmds.setParent('..')
+
+cmds.rowLayout('trd' , numberOfColumns=2 , columnAlign=(1, 'right'))
+cmds.button('open' , l='open' ,width=150 ,c = f.open)
+cmds.button('save' , l='save' ,width=150 , c = f.save)
 
 
 cmds.showWindow('saveWindow')
